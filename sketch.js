5a678ebf5e67d9ff94d61b6b443f6c078172be57
thiagohersan <thiago.hersan+github@gmail.com>
2023-09-13 16:08:15 -0400

    made :/ frog face

--- sketch.js
+++ sketch.js
@@ -1,15 +1,31 @@
+let eyeWidth = 100;
+let faceWidth = 300;
+let pupilWidth = 50;
+
 function setup() {
   createCanvas(400, 600);
   background(255, 0, 255);
 }
 
 function draw() {
+  fill("green");
+  stroke(0);
+  strokeWeight(1);
+  ellipse(200, 400, faceWidth, faceWidth);
   stroke(0);
   fill("orange");
-  ellipse(200, 300, 100, 100);
-  fill(0, 255, 0);
-  ellipse(300, 300, 200, 200);
-  stroke(255, 0, 0);
-  strokeWeight(4);
-  rect(300, 300, 200, 200);
+  ellipse(100, 300, eyeWidth, eyeWidth);
+  ellipse(300, 300, eyeWidth, eyeWidth);
+
+  fill("black");
+  ellipse(100, 300, pupilWidth/2, pupilWidth);
+  ellipse(300, 300, pupilWidth/2, pupilWidth);
+
+  fill(222);
+  noStroke();
+  arc(200, 300, 200, 200, 3 * HALF_PI - PI / 10, 3 * HALF_PI + PI / 10);
+
+  stroke(0);
+  strokeWeight(3);
+  line(100, 450, 250, 500);
 }
