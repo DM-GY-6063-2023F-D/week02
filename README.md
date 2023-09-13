945c7532b602f8578af73f5a84ab320c770a2c5a
thiagohersan <thiago.hersan+github@gmail.com>
2023-09-13 17:03:35 -0400

    2 frog faces

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# p5.js Template
+# HW 02
+
 
 This is a README file that can be used to describe and document your assignment.
 
--- sketch.js
+++ sketch.js
@@ -3,11 +3,36 @@ let faceWidth = 300;
 let pupilWidth = 50;
 
 function setup() {
-  createCanvas(400, 600);
-  background(255, 0, 255);
+  createCanvas(800, 600);
 }
 
 function draw() {
+  background(200, 0, 100);
+
+  fill("green");
+  stroke(0);
+  strokeWeight(1);
+  ellipse(200, 400, faceWidth, faceWidth);
+  stroke(0);
+  fill("orange");
+  ellipse(100, 300, eyeWidth, eyeWidth);
+  ellipse(300, 300, eyeWidth, eyeWidth);
+
+  fill("black");
+  ellipse(100, 300, pupilWidth/2, pupilWidth);
+  ellipse(300, 300, pupilWidth/2, pupilWidth);
+
+  fill(222);
+  noStroke();
+  arc(200, 300, 200, 200, 3 * HALF_PI - PI / 10, 3 * HALF_PI + PI / 10);
+
+  stroke(0);
+  strokeWeight(3);
+  line(100, 450, 250, 500);
+
+
+  translate(400, 0);
+
   fill("green");
   stroke(0);
   strokeWeight(1);
